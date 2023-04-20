6207a43596fa109f966b741bd50bcae4788305e5
Griffin Ansel <griffin.ansel@gmail.com>
2023-04-20 10:35:59 -0400

    John Fisher sucks.

--- fuckjohnfisher.py
+++ fuckjohnfisher.py
@@ -1,18 +1,33 @@
+import os
 import smtplib
 import time
 
 # Email and login details
 from_address = "griffin.ansel@gmail.com"
 to_address = "john.fisher@athletics.com"
-password = env.email_password
+sender_name = "Griffin Ansel"
+password = os.environ["email_password"]
 
 # SMTP server details for Gmail
 smtp_server = "smtp.gmail.com"
 smtp_port = 587
 
 # Message details
-subject = "Fuck you, you garbage human being"
-body = "Dear John Fisher, you took the A's from Oakland, and now you're not welcome here. Enjoy the desert, it's as barren as your heart."
+subject = "John Fisher, We Hate You"
+body = f"""Dear John Fisher,
+
+On behalf of the City of Oakland, let me tell you that you are no longer welcome. Rooted in Oakland, what a joke.
+You told us you would keep our team here, but instead you pull a "major league" and tank for a season, raise ticket and
+concession prices, then complain when fans don't come to your games. Maybe if you put half the effort into the team
+as you do in failing to run the companies your parents gave you, we'd actually have a winner. Enjoy the desert.
+When you have hungover visiting fans puking around your shiny new stadium and no A's fans in sight, maybe you'll
+remember this email. Or the one coming in 5 minutes.
+
+Sincerely,
+
+{sender_name}
+
+Oakland A's fan"""
 
 while True:
     try:
